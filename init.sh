fecd0db7297b6e4998a1dfec6db8d12b34026124
Ilya <belogoloviy.ilya@yandex.ru>
2018-10-06 20:02:44 +0300

    kr

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/gunicorn_wsgi.conf hello:wsgi_application
